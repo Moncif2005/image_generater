d32ec3d5ffa2e14c7a3853cc46e2b15892c21cb4
abdelilleh <143023531+Moncif2005@users.noreply.github.com>
2024-03-02 12:00:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const api = "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"
+
+
+const inp = document.getElementById('inp')
+const images = document.querySelector('.images')
+
+const getImage = async () => {
+
+    // make a request to openai
+    const methods = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${api}`
+        },
+        body:JSON.stringify(
+            {
+                "prompt":inp.value,
+                "n":3,
+                "size": "1024x1024"
+
+            }
+        )
+    };
+    const res = await fetch("https://api.openai.com/v1/images/generations", methods)
+
+    // parse the response as json
+
+    const data = await res.json()
+    const listImages = data.data;
+    images.innerHTML = ""
+    listImages.map(photo => {
+        const container = document.createElement("div")
+        images.append(container)
+        const img = document.createElement("img")
+        container.append(img)
+        img.src = photo.url
+    })
+}
+
